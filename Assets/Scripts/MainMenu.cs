34e4ccb01d34e3f0675840d89dcb488ee7db0bf6
gyan vadhel <gyanvadhel@gmail.com>
2024-02-21 20:49:44 +0530

    final3

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,30 +10,30 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("Start");
+        audioSource.Play();
     }
 
     public void QuitGame()
     {
         Application.Quit();
+        audioSource.Play();
     }
 
     public void SettingMenu()
     {
         SceneManager.LoadScene("ControlsMenu");
+        audioSource.Play();
     }
 
     public void Menu()
     {
         SceneManager.LoadScene("MainMenu");
+        audioSource.Play();
     }
 
     public void SureQuit()
     {
         SceneManager.LoadScene("Quit");
-    }
-
-    public void Buttons()
-    {
         audioSource.Play();
-    }    
+    }
 }
