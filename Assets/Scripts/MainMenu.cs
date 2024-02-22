ee7abceea7125d9474e33f063938a34eda1de950
gyan <gyanvadhel@gmail.com>
2024-02-22 21:21:26 +0530

    ready ig

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,35 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 { 
-  public AudioSource audioSource;
 
     public void PlayGame()
     {
         SceneManager.LoadScene("Start");
-        audioSource.Play();
     }
 
     public void QuitGame()
     {
         Application.Quit();
-        audioSource.Play();
     }
 
     public void SettingMenu()
     {
         SceneManager.LoadScene("ControlsMenu");
-        audioSource.Play();
     }
 
     public void Menu()
     {
         SceneManager.LoadScene("MainMenu");
-        audioSource.Play();
     }
 
     public void SureQuit()
     {
-        SceneManager.LoadScene("Quit");
-        audioSource.Play();
+        SceneManager.LoadScene("Quit");   
     }
 }
